77c14f1bc549f56f8ffaf736a3c622c949f57cc6
Rudo2204 <rudo2204@gmail.com>
2020-02-12 12:53:26 +0700

    Add root previllege to the commands

--- ci/texlive/texlive_install.sh
+++ ci/texlive/texlive_install.sh
@@ -17,11 +17,11 @@ fi
 
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
+sudo tlmgr install luatex
 
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # We specify the directory in which it is located texlive_packages
-tlmgr install $(sed 's/\s*#.*//;/^\s*$/d' ci/texlive/texlive_packages)
+sudo tlmgr install $(sed 's/\s*#.*//;/^\s*$/d' ci/texlive/texlive_packages)
 
 # Can also just specify like the following
 # texlive-latex-base is needed to run pdflatex
@@ -32,7 +32,7 @@ tlmgr install $(sed 's/\s*#.*//;/^\s*$/d' ci/texlive/texlive_packages)
 #  amsmath
 
 # Keep no backups (not required, simply makes cache bigger)
-tlmgr option -- autobackup 0
+sudo tlmgr option -- autobackup 0
 
 # Update the TL install but add nothing new
-tlmgr update --self --all --no-auto-install
+sudo tlmgr update --self --all --no-auto-install
